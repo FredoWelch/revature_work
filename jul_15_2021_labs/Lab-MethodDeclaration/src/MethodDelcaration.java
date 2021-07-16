3f0bddc6f4889448c450177dbb7b41bd1e930775
Fredo0720 <freddie.welch@revature.net>
2021-07-16 14:19:20 -0400

    Thursday July 15th2021 Labs

---
+++ jul_15_2021_labs/Lab-MethodDeclaration/src/MethodDelcaration.java
@@ -0,0 +1,20 @@
+
+public class MethodDelcaration {
+	
+	public static void main(String[] args) {
+		
+		MethodDelcaration md = new MethodDelcaration();
+		
+		md.talk();
+		md.talk();
+		md.talk();
+		
+		}
+	
+		public void talk() {
+			System.out.println("Inside the talk method");
+		}
+		
+	}
+
+
